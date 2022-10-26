eccdae76240cfb15a879e7f384a9c772133e9b44
drstrange20 <fatiha.94@mail.ru>
2022-10-26 16:07:26 +0300

    hw02.8

--- src/ru/otus/homeworks/hw02/Exercise03.java
+++ src/ru/otus/homeworks/hw02/Exercise03.java
@@ -1,15 +1,15 @@
 package ru.otus.homeworks.hw02;
 
 public class Exercise03 {
-    public static void change(int str) {
-        System.out.println(str);
-        str = 1;
-        System.out.println(str);
-    }
-
     public static void main(String[] args) {
         int str = 0;
         change(str);
+        System.out.println(str);
+    }
+
+    public static void change(int str) {
+        System.out.println(str);
+        str = 1;
         System.out.println(str);
     }
 }
