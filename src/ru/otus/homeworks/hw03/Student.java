c3d76b96d55d7e3b6b8a8dc4bab556623c24717a
drstrange20 <fatiha.94@mail.ru>
2022-10-28 21:54:52 +0300

    hw03.01

--- src/ru/otus/homeworks/hw03/Student.java
+++ src/ru/otus/homeworks/hw03/Student.java
@@ -1,10 +1,18 @@
 package ru.otus.homeworks.hw03;
 
+
+import static ru.otus.homeworks.hw03.Test.scanner;
+
+
 class Student {
     private static String name;
 
-    public void setName(String name) {
-        Student.name = name;
+    public static void setName() {
+        while (!scanner.hasNext("[А-Яа-я]+")) {
+            System.out.println("Вы ввели некорректное имя");
+            scanner.next();
+        }
+        //здесь
     }
 
     public static String getName() {
