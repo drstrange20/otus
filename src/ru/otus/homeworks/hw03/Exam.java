60719712688b0786b57480788e5eca2023bfe457
drstrange20 <fatiha.94@mail.ru>
2022-10-29 09:35:25 +0300

    hw03.02

--- src/ru/otus/homeworks/hw03/Exam.java
+++ src/ru/otus/homeworks/hw03/Exam.java
@@ -1,39 +1,38 @@
 package ru.otus.homeworks.hw03;
 
+import java.util.Scanner;
 
 // другие методы сканнера для приема букв и чисел
 // if catch если не от 1 до 4 ошибка, потом заново заходит в while
 // дженерики
 // Linked list
 public class Exam {
-    private static int wrongAnswersCount;
-    private static int rightAnswersCount;
+    int rightAnswersCount;
+    int wrongAnswersCount;
 
     public Exam(int rightAnswersCount, int wrongAnswersCount) {
-        Exam.rightAnswersCount = rightAnswersCount;
-        Exam.wrongAnswersCount = wrongAnswersCount;
+        this.rightAnswersCount = rightAnswersCount;
+        this.wrongAnswersCount = wrongAnswersCount;
     }
 
-    public static int getRightAnswersCount() {
+    public int getRightAnswersCount() {
         return rightAnswersCount;
     }
 
-    public static void setWrongAnswersCount(int wrongAnswersCount) {
-        Exam.wrongAnswersCount = wrongAnswersCount;
-    }
-
-    public static int getWrongAnswersCount() {
+    public int getWrongAnswersCount() {
         return wrongAnswersCount;
     }
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Test test = new Test();
 
         System.out.println("Добро пожаловать на экзамен, введите Ваше имя:");
-        Student.setName();
-        System.out.println(Student.getName() + ", Вам необходимо сдать тест, в котором 3 вопроса.\n");
+        String name = scanner.nextLine();
+        Student student = new Student(name);
+        System.out.println(student.getName() + ", Вам необходимо сдать тест, в котором 3 вопроса.\n");
         Exam exam = test.startTesting();
-        System.out.println("Количество верных ответов:" + getRightAnswersCount() + "\nКоличество неверных ответов:" + getWrongAnswersCount());
+        System.out.println("Количество верных ответов:" + exam.getRightAnswersCount() + "\nКоличество неверных ответов:" + exam.getWrongAnswersCount());
     }
+
 }
