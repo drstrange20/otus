89f06b7d6cd6a003bb662d0e28c6546aa843b118
drstrange20 <fatiha.94@mail.ru>
2022-10-30 13:55:54 +0300

    hw03.03

--- src/ru/otus/homeworks/hw03/Exam.java
+++ src/ru/otus/homeworks/hw03/Exam.java
@@ -5,21 +5,27 @@ import java.util.Scanner;
 // другие методы сканнера для приема букв и чисел
 // if catch если не от 1 до 4 ошибка, потом заново заходит в while
 // дженерики
-// Linked list
+// Linked list вместо массива
+// a и b - 4 модификатора доступа?
+// каждый вопрос вариант и правильный ответ - отдельный объект
+// проверка на имя
+// разбор примеров с каждого занятия
 public class Exam {
     private int rightAnswersCount;
     private int wrongAnswersCount;
 
-    public Exam(int rightAnswersCount, int wrongAnswersCount) {
+    Exam(int rightAnswersCount, int wrongAnswersCount) {
         this.rightAnswersCount = rightAnswersCount;
         this.wrongAnswersCount = wrongAnswersCount;
     }
 
     public int getRightAnswersCount() {
+
         return rightAnswersCount;
     }
 
     public int getWrongAnswersCount() {
+
         return wrongAnswersCount;
     }
 
@@ -31,7 +37,7 @@ public class Exam {
         String name = scanner.nextLine();
         Student student = new Student(name);
         System.out.println(student.getName() + ", Вам необходимо сдать тест, в котором 3 вопроса.\n");
-        Exam exam = test.startTesting();
-        System.out.println("Количество верных ответов:" + exam.getRightAnswersCount() + "\nКоличество неверных ответов:" + exam.getWrongAnswersCount());
+        Exam result = test.startTesting();
+        System.out.println("Количество верных ответов:" + result.getRightAnswersCount() + "\nКоличество неверных ответов:" + result.getWrongAnswersCount());
     }
 }
