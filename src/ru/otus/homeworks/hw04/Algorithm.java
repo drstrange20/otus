ad64d09714491466eeaa7afd2ef95e3b2e59bd74
drstrange20 <fatiha.94@mail.ru>
2022-11-07 21:19:26 +0300

    hw04.02

--- src/ru/otus/homeworks/hw04/Algorithm.java
+++ src/ru/otus/homeworks/hw04/Algorithm.java
@@ -1,39 +1,42 @@
 package ru.otus.homeworks.hw04;
 
-
 import java.util.ArrayList;
 import java.util.Date;
+// При количестве элементов больше 6. Отчего зависит. И если сделать обычный массив, изменится ли результат?
 
 public class Algorithm {
+    public static int x;
+
     public static void main(String[] args) {
-        ArrayList<Byte> arrayList = new ArrayList<>();
-        arrayList.add((byte) 2);
-        arrayList.add((byte) 3);
-        arrayList.add((byte) 5);
-        arrayList.add((byte) 4);
-        arrayList.add((byte) 1);
-        arrayList.add((byte) 6);
-        arrayList.add((byte) 7);
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(5);
+        arrayList.add(4);
+        arrayList.add(1);
+        arrayList.add(0);
 
         System.out.println(arrayList);
-        byte start = (byte) new Date().getTime();
+        int start = (int) new Date().getTime();
         ArrayList arrayList1 = doSort(arrayList);
-        byte end = (byte) new Date().getTime();
+        int end = (int) new Date().getTime();
+        System.out.println("Количество итераций: " + x++);
         System.out.println(arrayList1 + "\ndelta: " + (end - start) + " ms");
 
     }
-    public static ArrayList doSort(ArrayList<Byte> list) {
-        for (byte i = 0; i < list.size(); i++) {
+    public static ArrayList doSort(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size(); i++) {
             int index = (int)(Math.random() * list.size());
 
-            byte element = list.get(i);
+            int element = list.get(i);
 
             list.set(i, list.get(index));
             list.set(index, element);
 
         }
-        for (int i = 0; i < list.size() - 1; i++) {
-            if (list.get(i) > list.get(i+1)) {
+        for (int j = 0; j < list.size() - 1; j++) {
+            if (list.get(j) > list.get(j+1)) {
+                x++;
                 doSort(list);
             }
         }
---
+++ src/ru/otus/homeworks/hw04/Algorithm2.java
@@ -0,0 +1,44 @@
+package ru.otus.homeworks.hw04;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+
+public class Algorithm2 {
+    public static int bufferN;
+    public static void main(String[] args) {
+        ArrayList<Integer> firstArrayList = new ArrayList<>();
+        ArrayList<Integer> secondArrayList = new ArrayList<>();
+        for (int i = 0; i < 20001; i++) {
+            int rand = (int)(Math.random() * 1000000);
+            firstArrayList.add(rand);
+            secondArrayList.add(rand);
+        }
+
+        int start1 = (int) new Date().getTime();
+        Collections.sort(firstArrayList);
+        int end1 = (int) new Date().getTime();
+        System.out.println("Java Collections sort " + (end1 - start1) + " ms");
+
+
+        int start = (int) new Date().getTime();
+        doSortWithChoice(secondArrayList);
+        int end = (int) new Date().getTime();
+        System.out.println("Сортировка выбором " + (end - start) + " ms");
+    }
+
+    public static void doSortWithChoice(ArrayList arrayList) {
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            int min = (int) arrayList.get(i);
+            for (int j = i +1; j < arrayList.size(); j++) {
+                if (min > (int)arrayList.get(j)) {
+                    min = (int)arrayList.get(j);
+                    bufferN = j;
+                }
+            }
+            int buffer = (int)arrayList.get(i);
+            arrayList.set(i, min);
+            arrayList.set(bufferN, buffer);
+        }
+    }
+}
