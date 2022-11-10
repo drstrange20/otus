116be2db7491bad802375f328b6074d5e8636f98
drstrange20 <fatiha.94@mail.ru>
2022-11-10 20:10:00 +0300

    hw05.02

--- src/ru/otus/homeworks/hw05/map/Account.java
+++ src/ru/otus/homeworks/hw05/map/Account.java
@@ -1,4 +1,35 @@
 package ru.otus.homeworks.hw05.map;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Account {
+    private final List<Integer> bankAccounts;
+
+    public Account(List<Integer> bankAccounts) {
+        this.bankAccounts = bankAccounts;
+    }
+
+    public void setBankAccounts(int accountNo) {
+        this.bankAccounts.add(accountNo);
+    }
+
+    public List<Integer> getBankAccounts() {
+        return bankAccounts;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Account account = (Account) o;
+
+        return Objects.equals(bankAccounts, account.bankAccounts);
+    }
+
+    @Override
+    public int hashCode() {
+        return bankAccounts != null ? bankAccounts.hashCode() : 0;
+    }
 }
--- src/ru/otus/homeworks/hw05/map/Client.java
+++ src/ru/otus/homeworks/hw05/map/Client.java
@@ -1,4 +1,31 @@
 package ru.otus.homeworks.hw05.map;
 
+import java.util.Objects;
+
 public class Client {
+    final String fullName;
+    private int age;
+
+    public Client(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+
+        if (age != client.age) return false;
+        return Objects.equals(fullName, client.fullName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fullName != null ? fullName.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
 }
