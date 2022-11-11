976763fcd4aecfb2874e3d49c7534efd6fe9a060
drstrange20 <fatiha.94@mail.ru>
2022-11-11 15:59:52 +0300

    hw05.05

--- src/ru/otus/homeworks/hw05/map/Account.java
+++ src/ru/otus/homeworks/hw05/map/Account.java
@@ -1,36 +1,11 @@
 package ru.otus.homeworks.hw05.map;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class Account {
-    private List<Integer> bankAccounts;
+    final ArrayList<Integer> bankAccounts;
 
-    Account(List<Integer> bankAccounts) {
+    public Account() {
         bankAccounts = new ArrayList<>();
-    }
-
-    public void setBankAccounts(int accountNo) {
-        this.bankAccounts.add(accountNo);
-    }
-
-    public List<Integer> getBankAccounts() {
-        return bankAccounts;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Account account = (Account) o;
-
-        return Objects.equals(bankAccounts, account.bankAccounts);
-    }
-
-    @Override
-    public int hashCode() {
-        return bankAccounts != null ? bankAccounts.hashCode() : 0;
     }
 }
