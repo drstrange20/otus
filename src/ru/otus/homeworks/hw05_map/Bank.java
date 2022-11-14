976911a229357fb9ceade7a6ccd783f161c147fe
drstrange20 <fatiha.94@mail.ru>
2022-11-14 15:35:45 +0300

    hw05.10

--- src/ru/otus/homeworks/hw05_map/Bank.java
+++ src/ru/otus/homeworks/hw05_map/Bank.java
@@ -1,62 +1,86 @@
 package ru.otus.homeworks.hw05_map;
 
 import java.util.*;
-// возвращать результат из методов и main печатать
+
 public class Bank {
+    private final HashMap<Client, Account> hashMap;
+
+    public Bank() {
+        hashMap = new HashMap<>();
+    }
+
+    public void putToHashMap(Client client, Account account) {
+        hashMap.put(client, account);
+    }
+
+    public HashMap<Client, Account> getHashMap() {
+        return hashMap;
+    }
+
     public static void main(String[] args) {
         Client client1 = new Client("Рахимьянов Айнур Радикович", 1994);
         Client client2 = new Client("Рахимьянов Айнур Радикович", 2000);
         Client client3 = new Client("Петров Иван Григорьевич", 2001);
 
-        ArrayList<Integer> arrayList1 = new ArrayList<>();
-        ArrayList<Integer> arrayList2 = new ArrayList<>();
-        ArrayList<Integer> arrayList3 = new ArrayList<>();
+        Account account1 = new Account();
+        account1.addAccount(123);
+        account1.addAccount(124);
+        account1.addAccount(125);
 
-        arrayList1.add(123);
-        arrayList1.add(124);
-        arrayList1.add(125);
+        Account account2 = new Account();
+        account2.addAccount(126);
+        account2.addAccount(127);
 
-        arrayList2.add(126);
-        arrayList2.add(127);
+        Account account3 = new Account();
+        account3.addAccount(128);
+        account3.addAccount(129);
 
-        arrayList3.add(128);
-        arrayList3.add(129);
+        Bank bank = new Bank();
+        bank.putToHashMap(client1, account1);
+        bank.putToHashMap(client2, account2);
+        bank.putToHashMap(client3, account3);
 
-        HashMap<Client, ArrayList<Integer>> hashMap = new HashMap<>();
-        hashMap.put(client1, arrayList1);
-        hashMap.put(client2, arrayList2);
-        hashMap.put(client3, arrayList3);
+        Account account = getAccounts(bank.getHashMap());
+        if (account == null) {
+            System.out.println("Счета по данному клиенту не найдены.");
+        } else {
+            System.out.println("Найдены следующие счета:\n" + account.getBankAccounts());
+        }
 
-//        ArrayList<Integer> clientsAccount = getAccounts(hashMap);
-//        if (clientsAccount == null) {
-//            System.out.println("Счета по данному клиенту не найдены");
+//        Client client = findClient(bank.getHashMap());
+//        if (client.getFullName() == null) {
+//            System.out.println("Клиент по данному счету не найден.");
 //        } else {
-//            System.out.println("Найдены следующие счета:\n" + clientsAccount);
+//            System.out.println("Клиент найден!\nФИО: " + client.getFullName() + ", дата рождения: " + client.getBirthDate());
 //        }
-
-        findClient(hashMap);
     }
 
-    static ArrayList<Integer> getAccounts(HashMap<Client, ArrayList<Integer>> hashMap) {
-        System.out.println("Поиск счетов по клиентской базе");
-        Client client = new Client("Петров Иван Григорьевич", 2001);
+    static Account getAccounts(HashMap<Client, Account> hashMap) {
+        System.out.println("Поиск счетов по клиентской базе...");
+        Client client = new Client("Рахимьянов Айнур Радикович", 1994);
         return hashMap.get(client);
-
     }
 
 
-    static void findClient(HashMap<Client, ArrayList<Integer>> hashMap) {
-        System.out.println("Поиск клиента по номеру счета");
-        int accountNumber = 129;
+    static Client findClient(HashMap<Client, Account> hashMap) {
+        String name = null;
+        int birthDate = 0;
 
-        for (HashMap.Entry<Client, ArrayList<Integer>> entry : hashMap.entrySet()) {
+        System.out.println("Поиск клиента по номеру счета...");
+        Account account = new Account();
+        account.addAccount(128);
+
+        for (Map.Entry<Client, Account> entry : hashMap.entrySet()) {
             Client key = entry.getKey();
-            ArrayList<Integer> value = entry.getValue();
-            for (Integer integer: value) {
-                if (accountNumber == integer) {
-                    new Client(key.getFullName(), key.getBirthDate());
+            Account value = entry.getValue();
+            for (Integer integer : value.getBankAccounts()) {
+                if (account.getAccountNumber().equals(integer)) {
+                    name = key.getFullName();
+                    birthDate = key.getBirthDate();
+                    break;
                 }
             }
         }
+        return new Client(name, birthDate);
     }
 }
