8f9003acf7b9443d4b12db75dbeac51fd5599877
drstrange20 <fatiha.94@mail.ru>
2022-11-14 16:35:43 +0300

    hw05.11

--- src/ru/otus/homeworks/hw05_map/Account.java
+++ src/ru/otus/homeworks/hw05_map/Account.java
@@ -3,13 +3,13 @@ package ru.otus.homeworks.hw05_map;
 import java.util.ArrayList;
 
 public class Account {
-    private ArrayList<Integer> bankAccounts;
+    private final ArrayList<Integer> bankAccounts;
 
     public Account() {
 
         bankAccounts = new ArrayList<>();
     }
-    public void addAccount (Integer integer) {
+    public void addAccount (int integer) {
         bankAccounts.add(integer);
     }
 
@@ -17,9 +17,4 @@ public class Account {
 
         return bankAccounts;
     }
-    public Integer getAccountNumber() {
-
-        return bankAccounts.get(0);
-    }
-
 }
